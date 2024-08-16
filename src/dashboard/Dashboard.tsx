921177bfc71781ab873fa968e766a6d9dfb3b88c
Rosemary-Oti <otirosemary58@gmail.com>
2024-08-16 10:54:16 +0100

    Trigger new deployment

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -3,9 +3,11 @@ import React from 'react';
 const Dashboard: React.FC = () => {
   return (
     <div className="h-screen bg-indigo-500">
+
         <h1 className="text-4xl flex font-serif animate-bounce justify-center items-center pt-40 font-bold text-gray-100 text-center" >
             WELCOME TO OUR DASHBOARD!
         </h1>
+        
     </div>
   )
 }
